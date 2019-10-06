47a9ebff357db1e506c59fe6d4b3b927b55e83e6
Ian Jr. Won <iwonjr@hotmail.com>
2019-10-06 15:06:34 -0400

    updating flask file

--- app.py
+++ app.py
@@ -1,86 +1,152 @@
-import os
-import json
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
 
 
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/Canadian_Stats")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Stats = Base.classes
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
-@app.route("/consumerCredit")
-def consumerCredit():
-    consumer_path = ('./consumerCredit (complete)/consumerCredit.json')
-    with open(consumer_path, 'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
 
-@app.route('/cpiCanada')
-def cpiCanada():
-    cpi_path = ('./cpi (complete)/cpiCanada.json')
-    with open(cpi_path, 'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
+#################################################
+# Flask Routes
+#################################################
 
-@app.route('/cpiProvincial')
-def cpiProvincal():
-    cpiPro_path = ('./cpi (complete)/cpiProvincial.json')
-    with open(cpiPro_path, 'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
-    
-@app.route('/gdpByProvince')
-def gdpProvince():
-    gdp_path = ('./gdp (complete)/gdpByProvince.json')
-    with open(gdp_path,'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/canada_labour_market/wages<br/>"
+        f"/canada_labour_market/cpi_Canada<br/>"
+        f"/canada_labour_market/cpi_Provincial<br/>"
+        f"/canada_labour_market/employement_data_Canada<br/>"
+        f"/canada_labour_market/gini_index_Canada<br/>"
+        f"/canada_labour_market/gini_index_Provincial<br/>"
+        f"/canada_labour_market/population_per_Provincial_bothsexes<br/>"
+        f"/canada_labour_market/population_per_Provincial_female<br/>"
+        f"/canada_labour_market/population_per_Provincial_male<br/>"
+        f"/canada_labour_market/tax_revenue_Canada<br/>"
+        f"/canada_labour_market/tax_revenue_Provincial<br/>"
+        f"/canada_labour_market/unemployment_Canada<br/>"
+        f"/canada_labour_market/unemployment_per_Provincial<br/>"
+        f"/canada_labour_market/wages_Canada<br/>"
+    )
 
-@app.route('/giniIndexCanada')
-def giniIndexCanada():
-    gini_path = ('./giniIndexData (complete)/giniIndexCanada.json')
-    with open(gini_path, 'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
 
-@app.route('/giniIndexProvincial')
-def giniIndexProvincial():
-    giniPro_path = ('./giniIndexData (complete)/giniIndexProvincial.json')
-    with open(giniPro_path, 'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
+@app.route('/canada_labour_market/wages')
+def wages():
+    results = pd.read_sql('SELECT * FROM wages', engine)
 
-@app.route('/MoneySupply')
-def MoneySupply():
-    ms_path = ('./money_supply/M1_moneySupply.json')
-    with open(ms_path,'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
 
-@app.route('/populationData/BothSex')
-def populationData():
-    popbothsex_path = ('./populationData (complete)/PopPerProvBothSex.json')
-    with open(popbothsex_path,'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
+@app.route('/canada_labour_market/cpi_Canada')
+def cpi_Canada():
+    results = pd.read_sql('SELECT * FROM cpicanada', engine)
 
-@app.route('/populationData/Female')
-def populationFemale():
-    popfem_path = ('./populationData (complete)/popPerProvFemale.json')
-    with open(popfem_path, 'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
 
-@app.route('/populationData/Male')
-def populationMale():
-    popmale_path = ('./populationData (complete)/popPerProvMale.json')
-    with open(popmale_path,'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
+@app.route('/canada_labour_market/cpi_Provincial')
+def cpi_Provincial():
+    results = pd.read_sql('SELECT * FROM cpiprovincial', engine)
 
-@app.route('/wagesCanada')
-def wagesCanada():
-    wagesCanada_path = ('./wagesCanada (complete)/wagesCanada.json')
-    with open(wagesCanada_path, 'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    return jsonify(file_data)
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
 
-if __name__ == "__main__":
+@app.route('/canada_labour_market/employement_data_Canada')
+def employement_data_Canada():
+    results = pd.read_sql('SELECT * FROM employmentdata', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+@app.route('/canada_labour_market/gini_index_Canada')
+def gini_index_Canada():
+    results = pd.read_sql('SELECT * FROM giniindexcanada', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+@app.route('/canada_labour_market/gini_index_Provincial')
+def gini_index_Provincial():
+    results = pd.read_sql('SELECT * FROM giniindexprovincial', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+@app.route('/canada_labour_market/population_per_Provincial_bothsexes')
+def population_per_Provincial_bothsexes():
+    results = pd.read_sql('SELECT * FROM populationperprovbothsex', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+@app.route('/canada_labour_market/population_per_Provincial_female')
+def population_per_Provincial_female():
+    results = pd.read_sql('SELECT * FROM populationperprovfemale', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+@app.route('/canada_labour_market/population_per_Provincial_male')
+def population_per_Provincial_male():
+    results = pd.read_sql('SELECT * FROM populationperprovmale', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+@app.route('/canada_labour_market/tax_revenue_Canada')
+def tax_revenue_Canada():
+    results = pd.read_sql('SELECT * FROM taxrevenuecanada', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+@app.route('/canada_labour_market/tax_revenue_Provincial')
+def tax_revenue_Provincial():
+    results = pd.read_sql('SELECT * FROM taxrevenueprovincial', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+@app.route('/canada_labour_market/unemployment_Canada')
+def unemployment_Canada():
+    results = pd.read_sql('SELECT * FROM unemploymentcanada', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+@app.route('/canada_labour_market/unemployment_per_Provincial')
+def unemployment_per_Provincial():
+    results = pd.read_sql('SELECT * FROM unemploymentperprov', engine)
+
+    # Convert data into Json
+    return jsonify(results.to_dict(orient = 'records'))
+
+if __name__ == '__main__':
     app.run(debug=True)
